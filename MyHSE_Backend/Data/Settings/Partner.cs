eec2edc660e82acdda0c4692bde84e7f1d005054
Muskan <ankitsahay1990@gmail.com>
2023-10-23 23:53:19 -0700

    Added Settings directory

---
+++ MyHSE_Backend/Data/Settings/Partner.cs
@@ -0,0 +1,9 @@
+﻿namespace MyHSE_Backend.Data.Settings
+{
+    /// <summary>
+    ///  this table is from Table3 Sheet from provided excell file. 
+    /// </summary>
+    public class Partner
+    {
+    }
+}
